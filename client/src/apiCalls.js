2089c8c90eeefd6d58fd02ee542caaca7e85074e
Rahul-rkm <skyphyrkm@gmail.com>
2023-04-07 02:04:19 +0530

    test 1

--- client/src/apiCalls.js
+++ client/src/apiCalls.js
@@ -4,7 +4,9 @@ export const loginCall = async (userCredential, dispatch) => {
     dispatch({ type: "LOGIN_START" });
     let res;
     try {
+        console.log("Before Login request");
         res = await axios.post("/api/auth/login", userCredential);
+        console.log("After Login request");
         dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
         sessionStorage.setItem("currentUser", JSON.stringify(res.data));
         return res.data;
