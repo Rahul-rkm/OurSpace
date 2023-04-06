2e46998b9d8502736d183170383cc2f64764c19b
Rahul-rkm <skyphyrkm@gmail.com>
2023-04-07 02:08:54 +0530

    test 2

--- client/src/apiCalls.js
+++ client/src/apiCalls.js
@@ -11,6 +11,7 @@ export const loginCall = async (userCredential, dispatch) => {
         sessionStorage.setItem("currentUser", JSON.stringify(res.data));
         return res.data;
     } catch (err) {
+        console.log('res ERR : ', err);
         dispatch({ type: "LOGIN_FAILURE", payload: err });
         console.log('res : ', err?.response?.data?.message);
         return err?.response?.data;
